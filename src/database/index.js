aeb3581b77fd5496a72b980a802e02da9a2e192d
Eduardo Luiz <hello@eduardoluiz.dev>
2022-01-26 12:18:19 -0300

    feat: create postgres conection

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const { Client } = require('pg');
+
+const client = new Client({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+});
+
+client.connect();
+
+exports.query = async (query) => {
+  const { rows } = await client.query(query);
+  return rows;
+};
