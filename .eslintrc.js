1692cc9cffbe719a0fd7709df7d1563f0b209018
Eduardo Luiz <hello@eduardoluiz.dev>
2022-02-01 21:15:24 -0300

    feat: add error handler

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,6 @@ module.exports = {
     'no-promise-executor-return': 'off',
     'consistent-return': 'off',
     camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-const dotenv = require('dotenv');
+require('express-async-errors');
 
-dotenv.config();
 const express = require('express');
 
 const routes = require('./routes');
@@ -9,5 +8,9 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, req, res, next) => {
+  console.log(error);
+  res.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
